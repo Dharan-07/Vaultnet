913af6f8662b4d046583013ed6c67aa8870b687a
Dharan-07 <dharanishdharan2135@gmail.com>
2025-11-02 15:09:18 +0530

    commiting VaultToken.sol

--- contracts/VaultToken.sol
+++ contracts/VaultToken.sol
@@ -1,23 +1,12 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.20;
+pragma solidity ^0.8.17;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract VaultToken is ERC20, Ownable {
-    constructor(address initialOwner, uint256 initialSupply)
-        ERC20("VaultToken", "VLT")
-        Ownable(initialOwner)
-    {
-        _mint(initialOwner, initialSupply);
-    }
-
-    function mint(address to, uint256 amount) external onlyOwner {
-        _mint(to, amount);
-    }
-
-    function burn(address from, uint256 amount) external onlyOwner {
-        _burn(from, amount);
+    constructor(address initialAccount, uint256 initialSupply) ERC20("VaultToken", "VTK") {
+        _mint(initialAccount, initialSupply);
+        transferOwnership(initialAccount);
     }
 }
-
